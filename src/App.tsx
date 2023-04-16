cecc7af9fe4c06935e9f1a49c95e2e41a37af8a8
Daniel DeFlores <ddeflores2@gmail.com>
2023-04-16 18:50:45 -0400

    setup environment

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./App.css";
+
+function App(): JSX.Element {
+    return (
+        <div className="App">
+            <div>CISC275 Final Project</div>
+            <div>By Dan, Jess, and James</div>
+        </div>
+    );
+}
+
+export default App;
