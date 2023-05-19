52139b6e8ae46c9579a40e571cd5002eb1f664c0
ddeflores <94253526+ddeflores@users.noreply.github.com>
2023-05-19 00:33:44 -0400

    Update VideoComponent.tsx

--- src/components/VideoComponent.tsx
+++ src/components/VideoComponent.tsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Button, Form } from "react-bootstrap";
 import { useDrag } from "react-dnd";
 import { Video } from "../interfaces/VideoInterface";
+//import { Viewer } from "../interfaces/ViewerInterface";
 
 function VideoComponent({
     name,
@@ -19,6 +20,7 @@ function VideoComponent({
     updateCentralList,
     updateModeratorList,
     updateCreatorList,
+    updateWatchList,
     deleteCentralVid,
     deleteCreatorVid,
     deleteReviewVid,
@@ -27,8 +29,7 @@ function VideoComponent({
     index,
     role,
     dropdown,
-    currentViewer,
-    viewers
+    currentViewer
 }: {
     name: string;
     description: string;
@@ -45,6 +46,7 @@ function VideoComponent({
     updateCentralList: (vid: Video) => void;
     updateModeratorList: (vid: Video) => void;
     updateCreatorList: (vid: Video) => void;
+    updateWatchList: (vid: Video) => void;
     deleteCentralVid: (vid: Video) => void;
     deleteCreatorVid: (vid: Video) => void;
     deleteReviewVid: (vid: Video) => void;
@@ -59,9 +61,7 @@ function VideoComponent({
     role: string;
     dropdown: boolean;
     currentViewer: string;
-    viewers: Viewer[];
 }) {
-    // State to keep track of all current video attributes
     const [video, setVideo] = useState<Video>({
         name,
         description,
@@ -77,14 +77,6 @@ function VideoComponent({
         dropdown
     });
 
-    // State to keep track of if the user is editing in their watchlist
-    const [editMode, setEditMode] = useState<boolean>(false);
-    // Updates state of edit mode
-    function updateEditMode(event: React.ChangeEvent<HTMLInputElement>) {
-        setEditMode(event.target.checked);
-    }
-
-    // Deletes the video from all lists on the website
     function deleteFromSite(vid: Video) {
         deleteCentralVid(vid);
         deleteCreatorVid(vid);
@@ -92,37 +84,32 @@ function VideoComponent({
         deleteWatchVid(vid, index, false, currentViewer);
     }
 
-    // Updates visibility of description in current list when changed
     function updateDropdown() {
         const newVideo = { ...video, dropdown: !video.dropdown };
         setVideo(newVideo);
     }
 
-    // Updates the likes on a video in all lists when changed
     function updateLikes() {
         const newVideo = { ...video, likes: video.likes + 1 };
         setVideo(newVideo);
         updateCentralList(newVideo);
         updateModeratorList(newVideo);
+        updateWatchList(newVideo);
         updateCreatorList(newVideo);
     }
-
-    // Updates the visibility of recommended tab on video in all watchlist when changed
     function update() {
         const newVideo = { ...video, wantRecommended: !video.wantRecommended };
         setVideo(newVideo);
     }
-
-    // Updates the reported state on video in all lists when changed
     function updateReported() {
         const newVideo = { ...video, isReported: !video.isReported };
         setVideo(newVideo);
         updateCentralList(newVideo);
         updateModeratorList(newVideo);
+        updateWatchList(newVideo);
         updateCreatorList(newVideo);
     }
 
-    // All videos can be dragged. They can be dropped into user watchlists
     const [{ isDragging }, drag] = useDrag(() => ({
         type: "VIDEO",
         item: { name: video.name },
@@ -131,21 +118,15 @@ function VideoComponent({
         })
     }));
 
-    // State to keep track of if the textbox is shown or not
     const [textbox, setTextBox] = useState<boolean>(false);
-    // Flips visibility of textbox
     function showTextBox() {
         setTextBox(!textbox);
     }
-
-    // State to keep track of comment in textbox
     const [comments, setComments] = useState<string>("");
-    // Updates state of comments
     function updateComments(event: React.ChangeEvent<HTMLInputElement>) {
         setComments(event.target.value);
     }
 
-    // Updates comments on each video in all lists
     function updateCommentsOnLists() {
         const newVideo = {
             ...video,
@@ -155,30 +136,7 @@ function VideoComponent({
         updateCentralList(newVideo);
         updateModeratorList(newVideo);
         updateCreatorList(newVideo);
-    }
-
-    // State to keep track of genre and title in textbox
-    const [currentTitle, setCurrentTitle] = useState<string>("");
-    const [currentGenre, setCurrentGenre] = useState<string>("");
-
-    // Update edited title in textbox
-    function updateCurrentTitle(event: React.ChangeEvent<HTMLInputElement>) {
-        setCurrentTitle(event.target.value);
-    }
-    // Update edited title in watchlist
-    function updateWatchlistTitle() {
-        const newVideo = { ...video, name: currentTitle };
-        setVideo(newVideo);
-    }
-
-    // Update edited genre in textbox
-    function updateCurrentGenre(event: React.ChangeEvent<HTMLInputElement>) {
-        setCurrentGenre(event.target.value);
-    }
-    // Update edited genre in watchlist
-    function updateWatchlistGenre() {
-        const newVideo = { ...video, genre: currentGenre };
-        setVideo(newVideo);
+        updateWatchList(newVideo);
     }
 
     return (
@@ -291,14 +249,6 @@ function VideoComponent({
                             Reviewed✔️
                         </Button>
                     </span>
-                    <span hidden={role !== "creator"}>
-                        <div>
-                            Viewers:
-                            {viewers.map((viewer: Viewer) => (
-                                <ul key={viewer.username}>{viewer.username}</ul>
-                            ))}
-                        </div>
-                    </span>
                     <span hidden={!inWatchlist}>
                         <Button
                             onClick={() =>
@@ -315,35 +265,6 @@ function VideoComponent({
                         >
                             ❌
                         </Button>
-                        <Form.Switch
-                            type="switch"
-                            id="edit-mode-check"
-                            label="Enter Edit Mode"
-                            checked={editMode}
-                            onChange={updateEditMode}
-                        />
-                        <div hidden={!editMode}>
-                            <span>
-                                <Form.Label>New Title in your List:</Form.Label>
-                                <Form.Control
-                                    value={currentTitle}
-                                    onChange={updateCurrentTitle}
-                                ></Form.Control>
-                                <Button onClick={updateWatchlistTitle}>
-                                    Change Title
-                                </Button>
-                            </span>
-                            <span>
-                                <Form.Label>New Genre in your List:</Form.Label>
-                                <Form.Control
-                                    value={currentGenre}
-                                    onChange={updateCurrentGenre}
-                                ></Form.Control>
-                                <Button onClick={updateWatchlistGenre}>
-                                    Change Genre
-                                </Button>
-                            </span>
-                        </div>
                     </span>
                 </div>
             </div>
